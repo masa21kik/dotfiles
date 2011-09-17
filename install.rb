02da6c0ecd186099ea27b0985fbe07f32ef05b37
masa21kik <masa21kik@gmail.com>
2011-09-17 17:58:27 +0900

    smart-compileでコンパイル時にcpplintをかけるようにした

--- install.rb
+++ install.rb
@@ -2,22 +2,38 @@
 # -*- mode: ruby; coding: utf-8 -*-
 
 pwd = File.expand_path(File.dirname(__FILE__))
-install_files = [
-                 'zsh.d',
-                 'zshrc',
-                 'zshenv',
-                 'emacs.d',
-                 'screenrc',
-                 'irbrc',
-                ]
+dot_installs = [
+                'zsh.d',
+                'zshrc',
+                'zshenv',
+                'emacs.d',
+                'screenrc',
+                'irbrc',
+               ]
+local_installs = [
+                  'bin',
+                 ]
 
-install_files.each do |f|
+dot_installs.each do |f|
   target = File.expand_path("~/.#{f}")
   if File.exists?(target)
-    puts "File #{target} already exist. skipped"
+    puts "#{target} already exist. skipped"
     next
   end
   cmd = "ln -s #{pwd}/#{f} ~/.#{f}"
+  puts cmd
+  system(cmd)
+end
+
+local_installs.each do |f|
+  target_dir = File.expand_path("~/local")
+  Dir.mkdir(target_dir) unless File.exists?(target_dir)
+  target = "#{target_dir}/#{f}"
+  if File.exists?(target)
+    puts "#{target} already exist. skippd"
+    next
+  end
+  cmd = "ln -s #{pwd}/#{f} #{target}"
   puts cmd
   system(cmd)
 end
