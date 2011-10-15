530211e8b2a979810504f3e6fa97f41970d04c66
masa21kik <masa21kik@gmail.com>
2011-10-15 11:04:35 +0900

    ローカル設定をできるようにした

--- install.rb
+++ install.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # -*- mode: ruby; coding: utf-8 -*-
+require 'fileutils'
 
 puts `git submodule init && git submodule update`
 Dir.chdir('emacs.d/elisp/rinari') {
@@ -30,15 +31,31 @@ dot_installs.each do |f|
   system(cmd)
 end
 
-local_installs.each do |f|
-  target_dir = File.expand_path("~/local")
-  Dir.mkdir(target_dir) unless File.exists?(target_dir)
-  target = "#{target_dir}/#{f}"
-  if File.exists?(target)
-    puts "#{target} already exist. skippd"
-    next
-  end
-  cmd = "ln -s #{pwd}/#{f} #{target}"
-  puts cmd
-  system(cmd)
+local_installs.each do |dir|
+  target_dir = File.expand_path("~/local/#{dir}")
+  FileUtils.mkdir_p(target_dir) unless File.exists?(target_dir)
+  Dir.glob("#{dir}/*") {|path|
+    f = File.basename(path)
+    target = "#{target_dir}/#{f}"
+    if File.exists?(target)
+      puts "#{target} already exist. skippd"
+      next
+    end
+    cmd = "ln -s #{pwd}/#{dir}/#{f} #{target}"
+    puts cmd
+    system(cmd)
+  }
 end
+
+# local_installs.each do |f|
+#   target_dir = File.expand_path("~/local/#{f}")
+#   Dir.mkdir(target_dir) unless File.exists?(target_dir)
+#   target = "#{target_dir}/#{f}"
+#   if File.exists?(target)
+#     puts "#{target} already exist. skippd"
+#     next
+#   end
+#   cmd = "ln -s #{pwd}/#{f} #{target}"
+#   puts cmd
+#   system(cmd)
+# end
--- zshrc
+++ zshrc
@@ -1,3 +1,5 @@
 source ~/.zsh.d/zshrc
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+
+[[ -s "$HOME/.zsh_local" ]] && source "$HOME/.zsh_local"
