79a830f3f27ca989cc3fba20ce0fa1aa1714cf6a
masa21kik <masa21kik@gmail.com>
2014-06-11 00:01:03 +0900

    delete popwin setting

--- emacs.d/inits/30-popwin.el
+++
@@ -1,8 +0,0 @@
-;; -*- coding: utf-8 -*-
-;;;=====================================
-;; popwin
-;;;=====================================
-(require 'popwin)
-(setq display-buffer-function 'popwin:display-buffer)
-
-(push '("\*helm-mode.*" :regexp t :height 0.5) popwin:special-display-config)
